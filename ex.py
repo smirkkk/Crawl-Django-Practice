cc2199346c833e16a8bcd47946c9a97115bdc687
DESKTOP-74P3HC3\2015JunPhill <444.gkk@gmail.com>
2017-01-19 14:18:06 +0900

    no message

--- ex.py
+++ ex.py
@@ -6,3 +6,16 @@ if __name__ == '__main__':
     data = urllib.request.urlopen(URL).read()
 
     soup = BeautifulSoup(data, 'html.parser')
+
+    #print(soup.findAll(id='mArticle'))
+
+    th = soup.findAll('table')
+
+    for x in th:
+        string = x.get('class')
+        print(string)
+        if string[1] == "tbl_season":
+            print(x)
+        else:
+            break
+   # // *[ @ id = "mArticle"] / div / div[3] / div / table
