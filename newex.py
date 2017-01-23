06c9700fa66070f15a133ba6b2869a3606d3f853
SeokYoung Kim <crom@zzizily.com>
2017-01-24 04:17:44 +0900

    python string format 사용법 확인 해보기 데이터 DB 적재할 구조를 생각 해보기

--- newex.py
+++ newex.py
@@ -4,10 +4,13 @@ url = 'http://score.sports.media.daum.net/record/baseball/kbo/plrinf_bat_main.da
 respose = requests.get(url)
 html = etree.HTML(respose.text)
 
+
+# 단순 17 이라 고 해서 반복으로 추출 하기 보다는 그냥 0,1,2 ~ 16 해도 상관 없음
+# 이 데이터들을 DB 적재 하기 좋은 방법을 생각 해보기
 for x in range(17):
-    elements = html.xpath('//*[@id="mArticle"]/div/div[3]/div/table/thead/tr/th[%d]' % x)
-    if elements:
-        print(elements[0].text)
-    elements = html.xpath('//*[@id="mArticle"]/div/div[3]/div/table/tbody/tr/td[%d]' % x)
-    if elements:
-        print(elements[0].text)
+    # python string format 방법 확인 하기
+    # https://docs.python.org/3.5/library/stdtypes.html#str.format
+    titles = html.xpath('//*[@id="mArticle"]/div/div[3]/div/table/thead/tr/th[{}]'.format(x))
+    values = html.xpath('//*[@id="mArticle"]/div/div[3]/div/table/tbody/tr/td[{}]'.format(x))
+    if titles:
+        print(x, titles[0].text, values[0].text)
