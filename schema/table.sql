1976b736ad0a5c0477dd776e2c313793c7d2d472
DESKTOP-74P3HC3\2015JunPhill <444.gkk@gmail.com>
2017-01-31 21:09:08 +0900

    DB schema file

---
+++ schema/table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `table` (
+	`game` INT(11) NULL DEFAULT NULL,
+	`avg` DOUBLE NULL DEFAULT NULL,
+	`rbi` INT(11) NULL DEFAULT NULL
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
---
+++ schema/table3.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `table3` (
+	`date` DOUBLE NULL DEFAULT NULL,
+	`game` INT(11) NULL DEFAULT NULL
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
